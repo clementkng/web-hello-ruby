f59a7f9036322749f72431e632ae647f47a612df
Clement Ng <clementdng@gmail.com>
2021-05-04 23:15:07 -0700

    Added initial 'Hello, world' webserver
    Signed-off-by: Clement Ng <clementdng@gmail.com>

---
+++ webserver.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require "webrick"
+
+server = WEBrick::HTTPServer.new(:Port => 8000)
+
+server.mount_proc '/' do |req, res|
+  res.body = 'Hello, world!'
+end
+
+trap("INT") {
+    server.shutdown
+}
+
+server.start
